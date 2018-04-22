00498a6d0ef9e76ab6bad6fac24f9f265c96e318
brice <briceguard-git@yahoo.com>
2018-04-22 14:31:41 +0100

    add basic flex/bison logic

---
+++ src/align_main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "align_ast.h"
+
+AST * root;
+
+extern void yyparse();
+extern void parse_string( const char * str );
+
+using namespace std;
+void yyerror(char*s){
+	cout << "ERROR: " << s << endl;
+}
+
+int main(int argc, char * argv[] ){
+	parse_string( "2*3+2\n");
+	yyparse();
+
+	cout << "CPP: " << root->value() << endl;
+	
+	parse_string( "3*2+6\n");
+	yyparse();
+	
+	cout << "CPP: " << root->value() << endl;
+}
